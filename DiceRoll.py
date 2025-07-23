29bccf7b62bf6f46a1a9bba05133b9bfb39abf3b
godblesstaha <elomarielalaouitaha@gmail.com>
2025-07-23 21:43:18 +0100

    Dice roll project

---
+++ DiceRoll.py
@@ -0,0 +1,20 @@
+#DiceRoll
+import random
+
+choice = input("Do you want to roll the dice? (y/n): ")
+
+while choice != "y" and choice != "n":
+    print("Your choice is invalid!")
+    choice = input("Do you want to roll the dice? (y/n): ")
+
+while choice == "y":
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+    print(f"You rolled: ({x}, {y})")
+    choice = input("Do you want to roll again? (y/n): ")
+
+    while choice != "y" and choice != "n":
+        print("Your choice is invalid!")
+        choice = input("Do you want to roll again? (y/n): ")
+
+print("Thanks for playing the game!")
